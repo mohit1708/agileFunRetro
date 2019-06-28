1a4c7b5669272d9d71f64dec62b240d06883b537
mohit1708 <mohit.btechit@gmail.com>
2019-06-28 07:29:01 +0530

    Add files via upload
    Commited with new services

--- server.js
+++ server.js
@@ -4,102 +4,6 @@ const app = express();
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 const port = 3000
-const data = {
-	"wentWell": [{
-			"id": 123,
-			"title": "All Stories completed on Time",
-			"upCount": 0,
-			"downCount": 0
-		},
-		{
-			"id": 223,
-			"title": "Team Collaboration was good between QA and DEV in this Sprint",
-			"upCount": 0,
-			"downCount": 0
-		},
-		{
-			"id": 224,
-			"title": "Proper UT has been done from Dev Side which lead to 0 defect in  QA",
-			"upCount": 0,
-			"downCount": 0
-		}
-	],
-	"toImprove": [{
-			"id": 780,
-			"title": "Proper analysis need to be done before Sprint Planning.",
-			"upCount": 0,
-			"downCount": 0
-		},
-		{
-			"id": 7987,
-			"title": "Correct Estimation of Stories need to be given.",
-			"upCount": 0,
-			"downCount": 0
-		}, {
-			"id": 797,
-			"title": "Environment Issue need to be fix.",
-			"upCount": 0,
-			"downCount": 0
-		}
-	],
-	"actions": [{
-			"id": 798987,
-			"title": "Allocation of Environment.",
-			"upCount": 0,
-			"downCount": 0
-		},
-		{
-			"id": 798989,
-			"title": "Time need to be given for Analaysis",
-			"upCount": 0,
-			"downCount": 0
-		},
-		{
-			"id": 798990,
-			"title": "Allocation of task should be given as per interest and expertise",
-			"upCount": 0,
-			"downCount": 0
-		}
-	],
-	"bestContributer": [{
-			"id": 798327,
-			"title": "Vaibhav",
-			"upCount": 0,
-			"downCount": 0
-		},
-		{
-			"id": 79817,
-			"title": "Mohit",
-			"upCount": 0,
-			"downCount": 0
-		},
-		{
-			"id": 798427,
-			"title": "Gaurav",
-			"upCount": 0,
-			"downCount": 0
-		},
-		{
-			"id": 798328,
-			"title": "Vaqass",
-			"upCount": 0,
-			"downCount": 0
-		},
-		{
-			"id": 798330,
-			"title": "Preeti",
-			"upCount": 0,
-			"downCount": 0
-		},
-		{
-			"id": 798331,
-			"title": "Asif",
-			"upCount": 0,
-			"downCount": 0
-		}
-
-	]
-}
 
 var MongoClient = require('mongodb').MongoClient
 
@@ -128,7 +32,6 @@ app.post('/saveComments', (req, res) => {
 	  db.collection("comments").insertOne(req.body).then(response => res.status(200).json(response)).catch(error => console.error(error));
   });
   
-	
 });
 
 
@@ -142,17 +45,28 @@ app.post('/getComments', (req, res) => {
   });
 });
 
+
+app.post('/updateDislikes', (req, res) => {
+	
+  MongoClient.connect('mongodb://localhost:27017/', { useNewUrlParser: true })
+  .then(client => {
+    const db = client.db('agileretro');
+    const collection = db.collection('comments');
+	collection.update({"id":req.body.id},{$inc: {dislikes: 1}},{multi:true});
+	res.send({"success":"DisLike Updated successfully"});
+  });
+});
+
 app.post('/updateLikes', (req, res) => {
 	
   MongoClient.connect('mongodb://localhost:27017/', { useNewUrlParser: true })
   .then(client => {
     const db = client.db('agileretro');
     const collection = db.collection('comments');
-	collection.find({"sessionId":req.body.sessionId}).toArray().then(response => res.status(200).json(response)).catch(error => console.error(error));
-	collection.update({"sessionId":req.body.sessionId,"id":req.body.id},{$inc: {likes: 1}},{multi:true});
+	collection.update({"id":req.body.id},{$inc: {likes: 1}},{multi:true});
+	res.send({"success":"Like Updated successfully"});
   });
 });
-
 
 
 
